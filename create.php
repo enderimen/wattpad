d4c08872dde9a9093ac6f8e094b87e4379df3625
Ender İMEN <enderimen@hotmail.com>
2018-03-03 01:39:23 +0300

    Post share area designed.

---
+++ create.php
@@ -0,0 +1,28 @@
+
+    <?php include 'header.php'; ?>
+
+      <div class="container-story">
+        <div class="head">
+          <h1>My Stories</h1>
+          <form action="">
+            <input type="submit" value="+New Story" class="button">
+          </form>
+        </div>
+        <div class="content">
+          <img src="img/logo.png" alt="Cover Image" title="Cover" class="image-style">
+          <div class="story-detail">
+            <h3>Untitled Story</h3>
+            <p>Updated Date:</p>
+            <div class="story-other">
+              <span><i class="far fa-eye"></i></span>
+              <span><i class="fal fa-star"></i></span>
+              <span><i class="fal fa-comment"></i></span>
+            </div>
+          </div>
+          <div class="writing">
+            <input type="submit" value="New Part" class="button">
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
