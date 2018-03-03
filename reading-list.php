26216736ef04e29673c14e2e1e7419d02c803c47
Ender İMEN <enderimen@hotmail.com>
2018-03-03 16:09:06 +0300

    "My Reading List" page desingned

---
+++ reading-list.php
@@ -0,0 +1,36 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+	<div class="title">
+		<span>Library</span>
+	</div>
+
+	<div class="reading-list-content">
+		
+		<div class="library-cardview">
+			<img src="img/logo.png" alt="" height="260" width="200" id="cover-image">
+			<div class="reading-list-user-info">
+				<div class="reading-cardview-profile-name">
+					<span>Kitap Adı</span>
+					<span>Kullanıcı Adı</span>
+				</div>
+				<div class="reading-cardview-profile-photo">
+					<img src="img/logo.png" alt="" height="48" width="48">
+				</div>
+			</div>
+			<div class="reading-list-other">
+				<div class="reading-view">
+					<span><i class="fa fa-eye"></i></span>
+					<span>2.5M</span>
+				</div>
+				<div class="reading-star">
+					<span><i class="fa fa-star"></i></span>
+					<span>37.2K</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</script>
+</body>
