6516314d10cbf87814a0d1538e0181809640c427
Ender İMEN <enderimen@hotmail.com>
2018-03-04 15:11:07 +0300

    Carousel slider components added.

--- index.php
+++ index.php
@@ -1,11 +1,132 @@
     <?php include 'header.php'; ?>
 
     <div class="container">
-      <div class="discover-module">
-        <div class="discover-header">
-            <p>Önerilenler</p>
-        </div>
-      </div>
+    	<div class="discover-module">
+    		<div class="discover-header">
+    			<p>Önerilenler</p>
+    		</div>
+
+    		<div class="ca-container">
+    			<div class="ca-wrapper">
+    				<div class="ca-item ca-item-1">
+    					<div class="ca-item-main">
+    						<div class="ca-icon">
+    							<img src="img/book1.jpg" alt="" height="400" width="200">
+    						</div>
+    						<div class="slide-content">
+    							<span>Başlık</span>
+    							<span class="gray-text">Kullanıcı Adı</span>
+    							<div class="slider-other gray-text">
+    								<span><i class="fa fa-eye"></i></span>
+    								<span>120K</span>
+
+    								<span><i class="fa fa-star"></i></span>
+    								<span>9.1K</span>
+
+    								<span><i class="fa fa-list"></i></span>
+    								<span>129</span>
+    							</div>
+    							<div class="story-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quisquam quo ad autem. Dicta molestiae, consectetur sint odit laudantium </div>
+								<div class="slider-labels gray-text">
+									<div class="label">
+										features
+									</div>
+								</div>
+    						</div>
+    					</div>
+    				</div>
+    				<div class="ca-item ca-item-2">
+    					<div class="ca-item-main">
+    						<div class="ca-icon">
+    							<img src="img/book1.jpg" alt="" height="400" width="200">
+    						</div>
+    						<div class="slide-content">
+    							<span>Başlık</span>
+    							<span class="gray-text">Kullanıcı Adı</span>
+    							<div class="slider-other gray-text">
+    								<span><i class="fa fa-eye"></i></span>
+    								<span>120K</span>
+
+    								<span><i class="fa fa-star"></i></span>
+    								<span>9.1K</span>
+
+    								<span><i class="fa fa-list"></i></span>
+    								<span>129</span>
+    							</div>
+    							<div class="story-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quisquam quo ad autem. Dicta molestiae, consectetur sint odit laudantium </div>
+								<div class="slider-labels gray-text">
+									<div class="label">
+										features
+									</div>
+								</div>
+    						</div>
+    					</div>
+    				</div>
+    				<div class="ca-item ca-item-3">
+    					<div class="ca-item-main">
+    						<div class="ca-icon">
+    							<img src="img/book1.jpg" alt="" height="400" width="200">
+    						</div>
+    						<div class="slide-content">
+    							<span>Başlık</span>
+    							<span class="gray-text">Kullanıcı Adı</span>
+    							<div class="slider-other gray-text">
+    								<span><i class="fa fa-eye"></i></span>
+    								<span>120K</span>
+
+    								<span><i class="fa fa-star"></i></span>
+    								<span>9.1K</span>
+
+    								<span><i class="fa fa-list"></i></span>
+    								<span>129</span>
+    							</div>
+    							<div class="story-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quisquam quo ad autem. Dicta molestiae, consectetur sint odit laudantium </div>
+								<div class="slider-labels gray-text">
+									<div class="label">
+										features
+									</div>
+								</div>
+    						</div>
+    					</div>
+    				</div>
+    				<div class="ca-item ca-item-4">
+    					<div class="ca-item-main">
+    						<div class="ca-icon">
+    							<img src="img/book1.jpg" alt="" height="400" width="200">
+    						</div>
+    						<div class="slide-content">
+    							<span>Başlık</span>
+    							<span class="gray-text">Kullanıcı Adı</span>
+    							<div class="slider-other gray-text">
+    								<span><i class="fa fa-eye"></i></span>
+    								<span>120K</span>
+
+    								<span><i class="fa fa-star"></i></span>
+    								<span>9.1K</span>
+
+    								<span><i class="fa fa-list"></i></span>
+    								<span>129</span>
+    							</div>
+    							<div class="story-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla quisquam quo ad autem. Dicta molestiae, consectetur sint odit laudantium </div>
+								<div class="slider-labels gray-text">
+									<div class="label">
+										features
+									</div>
+								</div>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
     </div>
-  </body>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <!-- the jScrollPane script -->
+    <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+    <script type="text/javascript" src="js/jquery.contentcarousel.js"></script>
+    <script type="text/javascript">
+    	$('.ca-container').contentcarousel();
+    </script>
+</body>
 </html>
