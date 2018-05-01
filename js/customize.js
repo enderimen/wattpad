f284995a8a9924e9d401f3829d82da0cbaeeb2fa
Ender İMEN <enderimen@hotmail.com>
2018-05-01 15:29:42 +0300

    Photo Upload [Trigger js]

---
+++ js/customize.js
@@ -0,0 +1,16 @@
+$('#upload').click(function () {
+	 $("#file").trigger('click');
+	 //$('#kaydet').trigger('click');
+	 alert('Resminizi seçtikten sonra kaydet butonuna basmayı unutmayınız...');
+	 //kaydet butonu tuıklatılacak
+});
+
+$('#upload_bg').click(function () {
+	 $("#file_bg").trigger('click');
+	 alert('Resminizi seçtikten sonra kaydet butonuna basmayı unutmayınız...');
+});
+
+$('#story-upload').click(function () {
+	 $("#story-cover-file").trigger('click');
+	 alert('Resminizi seçtikten sonra kaydet butonuna basmayı unutmayınız...');
+});
