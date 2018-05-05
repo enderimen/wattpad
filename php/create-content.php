f2f7a7d4de769ec81473ce94c0898399eb00ca8c
Ender İMEN <enderimen@hotmail.com>
2018-05-05 20:28:54 +0300

    "create-content" css file edited.

--- php/create-content.php
+++ php/create-content.php
@@ -4,44 +4,38 @@
 <div class="container">
   <div class="create-content">
     <form action="transaction.php" method="POST">
-      <div class="story-cover">
-        <img src="../img/story_photo/story_photo.png" alt="" height="300" width="200" id="story-upload">
-        <input type="file" name="story-cover-file" id="story-cover-file" hidden="hidden">
-
-        <input type="submit" name="story-cover-save" value="Photo Save" class="button">
-      </div>
       <div class="story-info">
 
         <h2>Title</h2>
-        <input type="text" name="story-title" placeholder="Untitled Story" class="input">
+        <input type="text" name="story-title" placeholder="Untitled Story" class="input" required="required">
 
         <h2>Description</h2>
-        <textarea name="story-desc" id="" cols="50" rows="10"></textarea>
+        <textarea name="story-desc" id="" cols="50" rows="10" required="required"></textarea>
 
         <h2>Genre</h2>
-        <select name="select-genre" class="input">
+        <select name="select-genre" class="input" required="required">
           <option value="-1">Select a genre</option>
-          <option value="14">Action</option>
-          <option value="11">Adventure</option>
-          <option value="24">ChickLit</option>
-          <option value="6">Fanfiction</option>
-          <option value="3">Fantasy</option>
-          <option value="21">General Fiction</option>
-          <option value="23">Historical Fiction</option>
-          <option value="9">Horror</option>
-          <option value="7">Humor</option>
-          <option value="8">Mystery / Thriller</option>
-          <option value="16">Non-Fiction</option>
-          <option value="12">Paranormal</option>
-          <option value="2">Poetry</option>
-          <option value="19">Random</option>
-          <option value="4">Romance</option>
-          <option value="5">Science Fiction</option>
-          <option value="17">Short Story</option>
-          <option value="13">Spiritual</option>
-          <option value="1">Teen Fiction</option>
-          <option value="18">Vampire</option>
-          <option value="22">Werewolf</option>
+          <option value="Action">Action</option>
+          <option value="Adventure">Adventure</option>
+          <option value="ChickLit">ChickLit</option>
+          <option value="Fanfiction">Fanfiction</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="General Fiction">General Fiction</option>
+          <option value="Historical Fiction">Historical Fiction</option>
+          <option value="Horror">Horror</option>
+          <option value="Humor">Humor</option>
+          <option value="Mystery / Thriller">Mystery / Thriller</option>
+          <option value="Non-Fiction">Non-Fiction</option>
+          <option value="Paranormal">Paranormal</option>
+          <option value="Poetry">Poetry</option>
+          <option value="Random">Random</option>
+          <option value="Romance">Romance</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Short Story">Short Story</option>
+          <option value="Spiritual">Spiritual</option>
+          <option value="Teen Fiction">Teen Fiction</option>
+          <option value="Vampire">Vampire</option>
+          <option value="Werewolf">Werewolf</option>
         </select>
         <input type="submit" name="save-story-info" value="Save Story" class="button">
       </div>
