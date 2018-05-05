14ce0ffbd07bdc3248a73546be43c4cdd40ab696
Ender İMEN <enderimen@hotmail.com>
2018-05-05 20:32:01 +0300

    "profile content" has been pulled with php

--- php/profile.php
+++ php/profile.php
@@ -1,123 +1,198 @@
 <?php require_once 'config.php'; 
-  if ($_SESSION['session_control'] == true) {
-      include 'header.php';   ?>
+if ($_SESSION['session_control'] == true) {
+  include 'header.php';   
 
-<div class="timeline-image" style="background-position: bottom;background-image: url(<?=$data['background_image']?>);">
-  <img src="<?=$data['profile_photo']?>" alt="Avatar">
-  <h1><?=$data['full_name']?></h1>
-  <p>@<?=$data['user_name']?></p>
+  if(isset($_GET["uid"])) {
 
-  <div class="details">
-    <div class="post">
-      <span>1</span>
-      <div>Post</div>
-    </div>
-    <div class="reading-list">
-      <span>1</span>
-      <div>Reading List</div> 
-    </div>
-    <div class="followers">
-      <span>1</span>
-      <div>Followers</div>
-    </div>
-  </div>
-</div>
+    $control_query = mysqli_query($connection, "SELECT * FROM kullanicilar WHERE id = '".$_GET["uid"]."'");
+    $user_data = mysqli_fetch_array($control_query);
 
-<div class="container">
-  <div class="tab">
-    <button class="tablinks" onclick="openTab(event, 'about')">About</button>
-    <button class="tablinks" onclick="openTab(event, 'following')">Following</button>
-  </div>
+    $follower_query = mysqli_query($connection, "SELECT followingID , following_name FROM followers WHERE followerID  = '".$_GET["uid"]."'");
 
-  <div id="about" class="tabcontent show">
-    <div class="bio">
-      <p>İnsanların sizi tanımalarına izin verin.</p>
-      <form action="">
-        <input type="submit" value="Add Description" class="button">
-      </form>
-      <p>JoinedNovember 27, 2017</p>
-    </div>
+    $story_sql = mysqli_query($connection, "SELECT * FROM stories WHERE userID = '".$_GET["uid"]."'");
 
-    <div class="myStory">
-      <div class="header-mystory">
-        <h1>My Strories</h1>
-      </div>
-      <div class="storyDetails">
-        <div class="storyCover">
-          <img src="../img/logo.png" alt="Resimsiz">
-        </div>
-        <div class="story-feature">
-          <div class="story-title">
-            <h3>Untitled</h3>
+    // Okuma listesindeki toplam kitap sayısı
+    $read_story_count_sql = mysqli_query($connection, "SELECT count(readID) as read_story_count FROM reading_list WHERE readerID = '".$user_data['id']."'");
+    $read_story_count = mysqli_fetch_array($read_story_count_sql);
+
+    // Kişinin takip ettiği toplam kişi sayısı
+    $follower_count_sql = mysqli_query($connection, "SELECT count(id) as follower_count FROM followers WHERE followerID = '".$user_data['id']."'");
+    $follower_count = mysqli_fetch_array($follower_count_sql);
+
+    // Kişinin takip ettiği toplam kişi sayısı
+    $post_count_sql = mysqli_query($connection, "SELECT count(storyID) post_count FROM stories WHERE userID = '".$user_data['id']."'");
+    $post_count = mysqli_fetch_array($post_count_sql);
+
+    if ($control_query) {
+      ?>
+
+      <div class="timeline-image" style="background-position: bottom;background-image: url(<?=$user_data['background_image']?>);">
+        <img src="<?=$data['profile_photo']?>" alt="Avatar">
+        <h1><?=$user_data['full_name']?></h1>
+        <p>@<?=$user_data['user_name']?></p>
+
+        <div class="details">
+          <div class="post">
+            <span><?=$post_count['post_count']?></span>
+            <div><a href="#posts">Post</a></div>
           </div>
-          <div class="icons">
-            <span>.</span>
-            <span>.</span>
-            <span>.</span>
+          <div class="reading-list">
+            <span><?=$read_story_count['read_story_count']?></span>
+            <div><a href="reading-list.php?ruid=<?=$user_data['id']?>">Reading List</a></div>
+          </div>
+          <div class="followers">
+            <span><?=$follower_count['follower_count']?></span>
+            <div>Followers</div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 
-  <div id="following" class="tabcontent">
-    <div class="cardview">
-      <div class="card-cover">
-        <img id="card-cover-image" src="../img/logo.png" alt="">
-        <img id="card-profile-image" src="../img/logo.png" alt="">
-      </div>
-      <div class="card-content">
-        <span>Adı</span>
-        <span>Kullanıcı Adı</span>
-        <input type="button" value="Following" class="button green">
-        <div class="card-other">
-          <div class="card-works">
-            <span>8</span>
-            <span>Works</span>
+      <div class="container">
+        <div class="tab">
+          <button class="tablinks" onclick="openTab(event, 'about')">About</button>
+          <button class="tablinks" onclick="openTab(event, 'following')">Following</button>
+
+          <form action="transaction.php" method="POST">
+
+            <input type="text" hidden="hidden" name="followingID" value="<?=$user_data['id']?>"> 
+            <input type="text" hidden="hidden" name="following_name" value="<?=$user_data["user_name"]?>">
+            <input type="text" hidden="hidden" name="uid" value="<?=$_GET['uid']?>">       
+
+            <?php 
+            $isFollowing = mysqli_query( $connection , "SELECT * FROM followers WHERE followerID ='".$_SESSION['id']."' AND followingID = '".$_GET['uid']."'");
+
+            if ($_GET['uid'] != $_SESSION['id'] ) {
+              if (mysqli_num_rows($isFollowing) > 0) { ?> 
+              <input type="submit" class="button" name="unfollow" value="Unfollow">
+              <?php } else { ?>
+              <input type="submit" class="button" name="follow" value="Follow">
+              <?php } } ?>
+            </form>
           </div>
-          <div class="card-following">
-            <span>8</span>
-            <span>Following</span>
+
+          <div id="about" class="tabcontent show">
+            <div class="bio">
+              <p>İnsanların sizi tanımalarına izin verin.</p>
+              <a href="settings.php" class="button">Add Description</a>
+              <p>JoinedNovember 27, 2017</p>
+            </div>
+
+            <div class="story-list" id="posts">
+              <div class="myStory">
+                <div class="header-mystory">
+                  <br>
+                  <h3>Stories by <?=$user_data['full_name']?></h3>
+                  <br>
+                </div>
+
+                <?php while ($private_user_data = mysqli_fetch_array($story_sql)) { ?>
+
+                <div class="storyDetails">
+                  <div class="storyCover">
+                    <img src="<?=$private_user_data['story_photo']?>" alt="Resimsiz">
+                  </div>
+                  <div class="story-feature">
+                    <div class="story-title">
+                      <h3><a href="story-read.php?rstory_id=<?=$private_user_data['storyID']?>"><?=$private_user_data['story_title']?></a></h3>
+                    </div>
+                    <div class="icons">
+                      <span>.</span>
+                      <span>.</span>
+                      <span>.</span>
+                    </div>
+                    <br>
+                    <span><?=$private_user_data['story_desc']?></span>
+                  </div>
+                </div>
+
+              <?php } ?>
+              
+              </div>
+            </div>
           </div>
-          <div class="card-followers">
-            <span>8</span>
-            <span>Followers</span>
+
+          <div id="following" class="tabcontent">
+
+            <?php while ($follower_data = mysqli_fetch_array($follower_query)) {
+
+              $following_query = mysqli_query($connection, "SELECT * FROM kullanicilar WHERE id = '".$follower_data['followingID']."'");
+              $following_data = mysqli_fetch_array($following_query);
+              ?>
+              <div class="cardview">
+                <div class="card-cover">
+                  <img id="card-cover-image" src="<?=$following_data['background_image']?>" alt="">
+                  <img id="card-profile-image" src="<?=$following_data['profile_photo']?>" alt="">
+                </div>
+                <div class="card-content">
+                  <span><a href="profile.php?uid=<?=$follower_data['followingID']?>"><?=$following_data['full_name']?></a></span>
+                  <span>@<?=$follower_data['following_name']?></span>
+                  <form action="transaction.php" method="POST">
+
+                    <input type="text" name="followingID" value="<?=$follower_data['followingID']?>" hidden="hidden"> 
+                    <input type="text" name="following_name" value="<?=$follower_data['following_name']?>" hidden="hidden">
+                    <input type="text" name="uid" value="<?=$_GET["uid"]?>" hidden="hidden">
+
+                    <?php 
+                    $isFollowing = mysqli_query( $connection , "SELECT * FROM followers WHERE followerID ='".$_SESSION['id']."' AND followingID = '".$follower_data['followingID']."'");
+
+                    if ($isFollowing) { ?>
+                    <input type="submit" class="button" name="unfollow" value="Unfollow">
+                    <?php } else { ?>
+                    <input type="submit" class="button" name="follow" value="Follow">
+                    <?php } ?>
+
+                  </form>
+                  <div class="card-other">
+                    <div class="card-works">
+                      <span>8</span>
+                      <span>Works</span>
+                    </div>
+                    <div class="card-following">
+                      <span>8</span>
+                      <span>Following</span>
+                    </div>
+                    <div class="card-followers">
+                      <span>8</span>
+                      <span>Followers</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <?php } ?>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 
-<?php }else{
-  header("Location:login.php");
-}
-?>
+          <?php } 
+        } 
+      } else{
+        header("Location:login.php");
+      }
+      ?>
 
-<!--Javascript-->
-<script>
-  /*Profil Sayfası Tab Menu*/
+      <!--Javascript-->
+      <script>
+        /*Profil Sayfası Tab Menu*/
 
-  function openTab(evt, tabName) {
+        function openTab(evt, tabName) {
 
-    var i, tabcontent, tablinks;
+          var i, tabcontent, tablinks;
 
-    tabcontent = document.getElementsByClassName("tabcontent");
+          tabcontent = document.getElementsByClassName("tabcontent");
 
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+          for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+          }
 
-    tablinks = document.getElementsByClassName("tablinks");
+          tablinks = document.getElementsByClassName("tablinks");
 
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+          for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+          }
 
-    document.getElementById(tabName).style.display = "flex";
-    evt.currentTarget.className += " active";
-  }
-</script>
-
-</body>
-</html>
+          document.getElementById(tabName).style.display = "flex";
+          evt.currentTarget.className += " active";
+        }
+      </script>
+    </body>
+    </html>
