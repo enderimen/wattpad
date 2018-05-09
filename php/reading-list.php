20c7c40b4f10e44880fe9940664ac45fe582eed7
Ender İMEN <enderimen@hotmail.com>
2018-05-09 21:29:56 +0300

    the information was retrieved.

--- php/reading-list.php
+++ php/reading-list.php
@@ -14,20 +14,24 @@
 
 	<div class="reading-list-content">
 		
-		<?php while ($reading_stories = mysqli_fetch_array($reading_stories_sql)) { 
+		<?php 	if (mysqli_num_rows($reading_stories_sql) == 0) { ?>
+		
+		<p><br><br><br>Henüz hiçbir kitap bulunmamakta.</p>
+		
+		<?php }else{ while ($reading_stories = mysqli_fetch_array($reading_stories_sql)) { 
 		
 		$reading_stories_details_sql =  mysqli_query( $connection , "SELECT * FROM stories WHERE storyID = '".$reading_stories['readed_storyID']."'");
 		$reading_stories_details = mysqli_fetch_array($reading_stories_details_sql); 
 
-		$story_author_user_sql =  mysqli_query( $connection , "SELECT user_name, profile_photo  FROM kullanicilar WHERE id = '".$reading_stories['story_authorID']."'");
+		$story_author_user_sql =  mysqli_query( $connection , "SELECT user_name, profile_photo,id  FROM kullanicilar WHERE id = '".$reading_stories['story_authorID']."'");
 		$story_author_user = mysqli_fetch_array($story_author_user_sql); ?>
 
 		<div class="library-cardview">
 			<img src="<?=$reading_stories_details['story_photo']?>" alt="" height="260" width="200" id="cover-image">
 			<div class="reading-list-user-info">
 				<div class="reading-cardview-profile-name">
-					<span><?=$reading_stories_details['story_title']?></span>
-					<span><?=$story_author_user['user_name']?></span>
+					<span><a href="story-read.php?rstory_id=<?=$reading_stories['readed_storyID']?>"><?=$reading_stories_details['story_title']?></a></span>
+					<span><a href="profile.php?uid=<?=$story_author_user['id']?>"><?=$story_author_user['user_name']?></a></span>
 				</div>
 				<div class="reading-cardview-profile-photo">
 					<img src="<?=$story_author_user['profile_photo']?>" alt="" height="48" width="48">
@@ -45,7 +49,7 @@
 			</div>
 		</div>
 
-		<?php } ?>
+		<?php } } ?>
 	</div>
 </div>
 
