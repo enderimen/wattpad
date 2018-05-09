d615d7fe6cd2f5a3636351087b16e6ff2f730290
Ender İMEN <enderimen@hotmail.com>
2018-05-09 21:23:49 +0300

    Conditions added.

--- php/story-read.php
+++ php/story-read.php
@@ -6,10 +6,12 @@ if ($_SESSION['session_control'] == true) {
 
 	$story_branch_read = mysqli_fetch_array($story_branch_read_sql);
 
+	@$story_comment_pull_sql = mysqli_query( $connection , "SELECT * FROM comments WHERE storyID = '".$_GET['rstory_id']."'"); 
+
 	?>
 
 	<div class="container">
-		<form action="transaction.php" method="POST" class="mind flex-row">
+		<div class="mind flex-row">
 			
 			<div class="story-menus">
 				
@@ -19,6 +21,8 @@ if ($_SESSION['session_control'] == true) {
 				</div>
 				<?php } ?>
 			</div>
+
+
 			<div class="story-read-content">
 				<h2>Welcome</h2><br>
 
@@ -31,21 +35,72 @@ if ($_SESSION['session_control'] == true) {
 				<input type="text" value="<?=$story_branch_read['userID']?>" name="userID" hidden="hidden">
 				<input type="text" value="<?=$_GET['rstory_id']?>" name="rstoryID" hidden="hidden">
 
-				<?php 
-                    $isUnlist = mysqli_query( $connection , "SELECT * FROM reading_list WHERE readerID = '".$_SESSION['id']."' AND readed_storyID = '".$_GET['rstory_id']."'");
+				<div class="comment-star">
+					<div class="comment-star-head">
+						<?php 
+						$isUnlist = mysqli_query( $connection , "SELECT * FROM reading_list WHERE readerID = '".$_SESSION['id']."' AND readed_storyID = '".$_GET['rstory_id']."'");
 
-                    if (mysqli_num_rows($isUnlist) > 0) { ?>
-                    <input type="submit" value="Un read List" name="un_read_list" class="button">
-                    <?php } else { ?>
-                    <input type="submit" value="Add to readList" name="add_read_list" class="button">
-                    <?php } ?>
-                
+						if (mysqli_num_rows($isUnlist) > 0) { ?>
+						<form action="transaction.php" method="POST">
+							<input type="submit" value="Un read List" name="un_read_list">
+						<?php } else { ?>
+							<input type="submit" value="Add to Read List" name="add_read_list">
+						</form>
+						<?php } ?>	
+
+						<a href="transaction.php?str=<?=$_GET['rstory_id']?>"><img src="../img/void_star.png" alt="" class="star-like"></a>
+					</div>
+
+					<form action="transaction.php" method="POST">
+						<div class="comment-details">
+							<img src="../img/book1.jpg" alt="" height="64" width="64">
+							<input type="text" name="comment" placeholder="Yorum yaz..." class="input-comment">
+							<input type="submit" name="comment-button" class="comment-button" value="Gönder">
+						</div>
+					</form>
+
+					<div class="comment-show-content">
+						
+						<?php while ($story_comment_pull_data = mysqli_fetch_array($story_comment_pull_sql)) { 
+							$comment_author_sql     =  mysqli_query( $connection , "SELECT user_name , profile_photo FROM kullanicilar WHERE id = '".$story_comment_pull_data['comment_authorID']."'");
+							$comment_author_data  =  mysqli_fetch_array($comment_author_sql);
+							?>
+							<div class="comment-content">
+								<img src="<?=$comment_author_data['profile_photo']?>" alt="" height="48" width="48">
+								<div class="comment-content-details">
+									<p><?=$comment_author_data['user_name']?></p>
+									<p><?=$story_comment_pull_data['comment_date']?></p>
+									<p><?=$story_comment_pull_data['comment']?></p>
+								</div>							
+							</div>
+							<?php while () { ?>
+
+								<p class="comment-to-comment">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates veritatis quam voluptatem ea, ipsa culpa quasi</p>
+							
+							<?php } ?>
+							
+							<form action="transaction.php" method="POST">
+								<div class="comment-details comment-to-comment-details">
+									<input type="text" name="commentID" value="<?=$story_comment_pull_data['commentID']?>" hidden="hidden">
+									<input type="text" name="rstoryID" value="<?=$_GET['rstory_id']?>" hidden="hidden">
+									
+									<img src="../img/book1.jpg" alt="" height="48" width="48">
+									
+									<input type="text" name="comment_to" class="input-comment" placeholder="Yorum yaz...">
+									<input type="submit" name="comment-to-comment-button" class="comment-button" value="Yanıtla">
+								</div>
+							</form>
+
+							<?php } ?>
+
+						</div>
+					</div>
+				</div>
 			</div>
-		</form>
-	</div>
+		</div>
 
-	<?php } else {
-		header("Location:login.php");
-	} ?>  
-</body>
-</html>
+		<?php } else {
+			header("Location:login.php");
+		} ?>  
+	</body>
+	</html>
