db58e85d19e67e19978edf818160d0754d60eaa2
Ender İMEN <enderimen@hotmail.com>
2018-05-26 19:04:06 +0300

    Header page

---
+++ admin/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Wattpad</title>
+
+    <!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+       <!--CUSTOM BASIC STYLES-->
+    <link href="assets/css/basic.css" rel="stylesheet" />
+    <!--CUSTOM MAIN STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+    <!-- GOOGLE FONTS-->
+     <!--Shortcut icon-->
+    <link rel="shortcut icon" href="assets/img/icon.png" type="image/x-icon" />
+
+    <!--Main Styles-->
+    <link href="assets/css/main.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Menü</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Wattpad</a>
+            </div>
+
+            <div class="header-right">
+                <a href="logout.php" class="btn btn-danger" title="Logout"><i class="fa fa-times fa-2x"></i></a>
+            </div>
+        </nav>
+        <!-- /. NAV TOP  -->
