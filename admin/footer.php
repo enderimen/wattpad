384c6fb2e5e0a33471822984caa211e628d6267f
Ender İMEN <enderimen@hotmail.com>
2018-05-26 19:03:57 +0300

    Footer page

---
+++ admin/footer.php
@@ -0,0 +1,15 @@
+ <div id="footer-sec">
+        &copy; 2018 | <a href="http://www.wattpad.com/" target="_blank">Wattpad.com</a>
+    </div>
+    <!-- /. FOOTER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+       <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
